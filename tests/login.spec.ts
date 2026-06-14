b587dcb751dfbdbbcc810ac3a9c35ed3fdf92cf2
RAM <ramkishorer162@gmail.com>
2026-06-14 09:29:22 +0530

    Updated Playwright test cases

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -13,20 +13,10 @@ test('Valid Login', async ({ page }) => {
 });
 test('Invalid Login', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-
   await page.locator('[data-test="username"]').fill(loginData.invalidUser.username);
   await page.locator('[data-test="password"]').fill(loginData.invalidUser.password);
 
-  await page.locator('[data-test="login-button"]').click();
-
-  await expect(page.locator('[data-test="error"]')).toBeVisible();
-});
-test('Empty Username', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-
-  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="login-button"]').click(); 
 
   await expect(page.locator('[data-test="error"]')).toBeVisible();
 });
@@ -35,6 +25,17 @@ test('Empty Password', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
   await page.locator('[data-test="username"]').fill('standard_user');
+
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+
+
+test('Empty username', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.locator('[data-test="password"]').fill('secret_sauce');
 
   await page.locator('[data-test="login-button"]').click();
 
